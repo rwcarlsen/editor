17c611abebd17b40152cea9d8b78eeff6931dcf0
Robert Carlsen <rwcarlsen@gmail.com>
2014-03-23 14:56:56 -0500

    refactored start line,char finding to a separate function

--- view/view.go
+++ view/view.go
@@ -6,46 +6,6 @@ import (
 	"github.com/rwcarlsen/editor/util"
 )
 
-type Tabber struct {
-	Line     []rune
-	Tabwidth int
-	VisLen   int
-	// ChToX returns the effective position of a rune indexed by the key if all tabs
-	// in the line were expanded to spaces of the Tabber's Tabwidth.
-	ChToX []int
-	// XToCh does the reverse of ChToX
-	XToCh []int
-}
-
-func NewTabber(line []rune, tabw int) *Tabber {
-	vislen := len(line) + strings.Count(string(line), "\t")*(tabw-1)
-	t := &Tabber{
-		Line:     line,
-		Tabwidth: tabw,
-		VisLen:   vislen,
-		ChToX:    make([]int, len(line)),
-		XToCh:    make([]int, vislen),
-	}
-
-	n := 0
-	for i, r := range line {
-		t.ChToX[i] = n
-		t.XToCh[n] = i
-		if r == '\t' {
-			for j := 0; j < t.Tabwidth; j++ {
-				t.XToCh[n+j] = i
-			}
-			n += t.Tabwidth
-			t.XToCh[n-1] = i
-			t.ChToX[i] = n - 1
-		} else {
-			n++
-		}
-	}
-
-	return t
-}
-
 type View interface {
 	Render() Surface
 	SetSize(w, h int)
@@ -137,26 +97,6 @@ func (c *WrapSurf) Y(line, char int) int {
 	return -1
 }
 
-// RenderLine renders the given line starting at the rune indexed by startch
-// and returns a slice of length w of indices that index into line for each x
-// tile. Indices of -1 indicate that no character is drawn.  nextch indexes the
-// first rune in line that didn't fit across the screen.
-func RenderLine(line []rune, startch int, w, tabw int) (chs []int, nextch int) {
-	chs = make([]int, w)
-	nextch = startch
-
-	t := NewTabber(line[startch:], tabw)
-	for x := 0; x < w; x++ {
-		if x < len(t.XToCh) {
-			chs[x] = startch + t.XToCh[x]
-			nextch = chs[x] + 1
-		} else {
-			chs[x] = -1
-		}
-	}
-	return chs, nextch
-}
-
 func (c *WrapSurf) init(w, h int, b *util.Buffer, startl, starty int, tabw int) {
 	c.b = b
 	c.lines = PosMap{}
@@ -165,19 +105,7 @@ func (c *WrapSurf) init(w, h int, b *util.Buffer, startl, starty int, tabw int) 
 	c.ys = PosMap{}
 
 	// figure out line+char for top left corner of canvas
-	l, nextch := startl, 0
-	y := starty
-	for l > 0 && y > 0 {
-		l--
-		line := b.Line(l)
-		t := NewTabber(line, tabw)
-		dy := t.VisLen/w + 1
-		y -= dy
-		nextch = 0
-		if y < 0 && t.VisLen > w {
-			nextch = t.XToCh[w*-1*y]
-		}
-	}
+	l, nextch := FindStart(b, w, startl, starty, tabw)
 
 	// draw from start line and char down
 	for y := 0; y < h; y++ {
@@ -215,3 +143,81 @@ func (c *WrapSurf) init(w, h int, b *util.Buffer, startl, starty int, tabw int) 
 		}
 	}
 }
+
+// RenderLine renders the given line starting at the rune indexed by startch
+// and returns a slice of length w of indices that index into line for each x
+// tile. Indices of -1 indicate that no character is drawn.  nextch indexes the
+// first rune in line that didn't fit across the screen.
+func RenderLine(line []rune, startch int, w, tabw int) (chs []int, nextch int) {
+	chs = make([]int, w)
+	nextch = startch
+
+	t := NewTabber(line[startch:], tabw)
+	for x := 0; x < w; x++ {
+		if x < len(t.XToCh) {
+			chs[x] = startch + t.XToCh[x]
+			nextch = chs[x] + 1
+		} else {
+			chs[x] = -1
+		}
+	}
+	return chs, nextch
+}
+
+func FindStart(b *util.Buffer, w, startl, starty int, tabw int) (line, char int) {
+	line, char = startl, 0
+	y := starty
+	for line > 0 && y > 0 {
+		line--
+		l := b.Line(line)
+		t := NewTabber(l, tabw)
+		dy := t.VisLen/w + 1
+		y -= dy
+		char = 0
+		if y < 0 && t.VisLen > w {
+			char = t.XToCh[w*-1*y]
+		}
+	}
+	return line, char
+}
+
+type Tabber struct {
+	Line     []rune
+	Tabwidth int
+	VisLen   int
+	// ChToX returns the effective position of a rune indexed by the key if all tabs
+	// in the line were expanded to spaces of the Tabber's Tabwidth.
+	ChToX []int
+	// XToCh does the reverse of ChToX
+	XToCh []int
+}
+
+func NewTabber(line []rune, tabw int) *Tabber {
+	vislen := len(line) + strings.Count(string(line), "\t")*(tabw-1)
+	t := &Tabber{
+		Line:     line,
+		Tabwidth: tabw,
+		VisLen:   vislen,
+		ChToX:    make([]int, len(line)),
+		XToCh:    make([]int, vislen),
+	}
+
+	n := 0
+	for i, r := range line {
+		t.ChToX[i] = n
+		t.XToCh[n] = i
+		if r == '\t' {
+			for j := 0; j < t.Tabwidth; j++ {
+				t.XToCh[n+j] = i
+			}
+			n += t.Tabwidth
+			t.XToCh[n-1] = i
+			t.ChToX[i] = n - 1
+		} else {
+			n++
+		}
+	}
+
+	return t
+}
+
